0f1b22bf452e50fd7f36fb76075df56f6c21200c
aozodbek2197 <aozodbek2197@gmail.com>
2026-01-06 14:49:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# # 1-misol
+# nums = [1, 2, 3, 4]
+# result = list(map(lambda x: x**2, nums))
+# # 2-misol
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# result = list(map(lambda x, y: x + y, a, b))
+# # 3-misol
+# words = ["apple", "banana"]
+# result = list(map(lambda x: x.upper(), words))
+# # 4-misol
+# nums = [1, 2, 3, 4, 5, 6]
+# result = list(filter(lambda x: x % 2 == 0, nums))
+# # 5-misol
+# words = ["apple", "banana", "pear", "grapes"]
+# result = list(filter(lambda x: len(x) > 5, words))
